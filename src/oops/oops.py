b53682b324cef866ad4f9ca178f3c9816b5517a1
Ramesh Nataraj <rameshnraj1822@gmail.com>
2024-06-25 18:33:40 +0530

    updated oops commit

--- src/oops/oops.py
+++ src/oops/oops.py
@@ -1,54 +1,51 @@
-
 class Student:
     # --> Constructor Method
-    def __init__(self):
-        pass
+    def __init__(self, names, total_mark):
+        self.total_marks = total_mark
+        self.name = names
 
     ## Method
-    def result(self, total_marks: int):
+    def result(self):
 
-        if total_marks > 50:
+        if self.total_marks > 50:
             return "pass"
         else:
             return "fail"
 
     ## Main Method
-    def main(self, name, total_mark):
-        res = self.result(total_mark)
+    def main(self):
+        res = self.result()
         return name, res
 
 
 ## Inheritance
 class Grade(Student):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, names, total_mark):
+        super().__init__(names, total_mark)
+        self.total_marks = total_mark
 
-    def grading(self, total_mark: int):
-        if total_mark > 90:
+    def main(self):
+        if self.total_marks > 90:
             return "Grade A+"
-        if 75 < total_mark < 90:
+        if 75 < self.total_marks < 90:
             return "Grade A"
-        if 60 < total_mark < 75:
+        if 60 < self.total_marks < 75:
             return "Grade B"
-        if 50 < total_mark < 60:
+        if 50 < self.total_marks < 60:
             return "Grade C"
         else:
             return "Grade D"
-
-    def main(self, total_marks):
-        res = self.grading(total_marks)
-        return res
 
 
 # control Statement
 if __name__ == "__main__":
     # object
     name = input("enter your name")
-    total_mark = int(input("enter your marks"))
+    total_marks = int(input("enter your marks"))
     ## object
-    obj = Student()
-    obj1 = obj.main(name, total_mark)
-    grad = Grade()
-    grad1 = grad.main(total_mark)
-    print(obj1)
-    print(grad1)
+    student = Student(name, total_marks)
+    student1 = student.main()
+    grade = Grade(name, total_marks)
+    grade1 = grade.main()
+    print(student1)
+    print(grade1)
