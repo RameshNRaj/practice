605d9afceb5a9639b580b8eb9db8587c580adac2
Ramesh Nataraj <rameshnraj1822@gmail.com>
2024-06-27 12:09:51 +0530

    100% coverage report commit

--- src/oops/oops.py
+++ src/oops/oops.py
@@ -1,37 +1,29 @@
 class Student:
     # --> Constructor Method
-    def __init__(self, names, total_mark):
-        self.total_marks = total_mark
-        self.name = names
+    def __init__(self):
+        pass
 
     ## Method
-    def result(self):
-
-        if self.total_marks > 50:
+    def main(self, total_marks):
+        if total_marks >= 50:
             return "pass"
         else:
             return "fail"
 
-    ## Main Method
-    def main(self):
-        res = self.result()
-        return name, res
-
 
 ## Inheritance
 class Grade(Student):
-    def __init__(self, names, total_mark):
-        super().__init__(names, total_mark)
-        self.total_marks = total_mark
+    def __init__(self):
+        super().__init__()
 
-    def main(self):
-        if self.total_marks > 90:
+    def main(self, total_mark):
+        if total_mark > 90:
             return "Grade A+"
-        if 75 < self.total_marks < 90:
+        if 75 < total_mark < 90:
             return "Grade A"
-        if 60 < self.total_marks < 75:
+        if 60 < total_mark < 75:
             return "Grade B"
-        if 50 < self.total_marks < 60:
+        if 50 < total_mark < 60:
             return "Grade C"
         else:
             return "Grade D"
@@ -40,12 +32,13 @@ class Grade(Student):
 # control Statement
 if __name__ == "__main__":
     # object
-    name = input("enter your name")
+    names = input("enter your name")
     total_marks = int(input("enter your marks"))
     ## object
-    student = Student(name, total_marks)
-    student1 = student.main()
-    grade = Grade(name, total_marks)
-    grade1 = grade.main()
+    student = Student()
+    student1 = student.main(total_marks)
+    grade = Grade()
+    grade1 = grade.main(total_marks)
+    print(names)
     print(student1)
     print(grade1)
--- tests/oops/test_oops.py
+++ tests/oops/test_oops.py
@@ -1,9 +1,31 @@
 import unittest
-from oops.oops import Grade
+from oops.oops import Grade, Student
+
 
 class TestOops(unittest.TestCase):
 
+    def test_student(self):
+        student = Student()
+        self.assertEqual(student.main(51), "pass")
+        self.assertEquals(student.main(45), "fail")
+
     def test_grading(self):
         grade = Grade()
-        output = grade.main(20)
+        # output = grade.main(20)
+        self.assertEquals(grade.main(91), "Grade A+")
 
+    def test_grading1(self):
+        grade = Grade()
+        self.assertEquals(grade.main(80), "Grade A")
+
+    def test_grading2(self):
+        grade = Grade()
+        self.assertEquals(grade.main(70), "Grade B")
+
+    def test_grading3(self):
+        grade = Grade()
+        self.assertEquals(grade.main(55), "Grade C")
+
+    def test_grading4(self):
+        grade = Grade()
+        self.assertEquals(grade.main(45), "Grade D")
